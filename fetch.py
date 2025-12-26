464a1af00b9f6d23b676dee4693ec1706994052b
Piotr Żarczyński <86186514+pzarczynski@users.noreply.github.com>
2025-12-26 12:49:43 +0000

    add fetching tool

---
+++ fetch.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+
+import requests
+from tqdm.auto import tqdm
+
+
+def fetch_url(url, out: str, block_size=1024):
+    r = requests.get(url, stream=True)
+    r.raise_for_status()
+
+    total_size = int(r.headers.get("content-length", 0))
+    n_blocks = (total_size + block_size - 1) // block_size
+
+    with open(out, "wb") as f:
+        for data in tqdm(r.iter_content(block_size), total=n_blocks, unit="KB"):
+            f.write(data)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d",
+        "--dataset",
+        type=str,
+        default="default",
+        choices=["default", "desync50", "desync100", "raw"],
+    )
+    parser.add_argument("-o", "--out-dir", type=str, default=".")
+    args = parser.parse_args()
+
+    os.makedirs(args.out_dir, exist_ok=True)
+
+    filename = {
+        "default": "ASCAD.h5",
+        "desync50": "ASCAD_desync50.h5",
+        "desync100": "ASCAD_desync100.h5",
+        "raw": "ATMega8515_raw_traces.h5",
+    }.get(args.dataset)
+
+    url = f"https://huggingface.co/datasets/zarczynski/ASCAD/resolve/main/{filename}"
+    path = os.path.join(args.out_dir, filename)
+
+    fetch_url(url, path)
